246b0cff495a5ab23234d7d0b07f647cb8b598a3
Ana Beatriz <anabneo_@outlook.com>
2019-07-14 17:41:16 -0300

    lint format

--- app/src/main/java/com/example/conversordemoedas/MainActivity.java
+++ app/src/main/java/com/example/conversordemoedas/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.conversordemoedas;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+        this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    private static class ViewHolder {
+        EditText editValue;
+        TextView textDolar;
+        TextView textEuro;
+        Button buttonCalculate;
+
+
+    }
+
 }
